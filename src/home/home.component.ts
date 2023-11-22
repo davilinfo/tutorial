46112e4ee77482a016ef0da86b69f8c409979afe
Davi <davilinfo@gmail.com>
2023-11-21 22:39:48 -0300

    angular 17

---
+++ src/home/home.component.ts
@@ -0,0 +1,35 @@
+import {Component, inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HousingLocation } from '../housing-location/housing-location';
+import { RouterOutlet } from '@angular/router';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingService } from '../housing-location/housing-service';
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    imports: [CommonModule, HousingLocationComponent],
+    templateUrl: './home.component.html',
+    styleUrls: ['home.component.css']
+})
+
+export class HomeComponent {    
+    housingLocationList: HousingLocation[] = [];
+    housingService: HousingService = inject(HousingService);
+    filteredLocationList: HousingLocation[] = [];
+
+    constructor(){
+        this.housingLocationList = this.housingService.getAllHousingLocations();
+        this.filteredLocationList = this.housingLocationList;
+    }
+
+    filterResults(text: string) {
+        if (!text) {
+          this.filteredLocationList = this.housingLocationList;
+        }
+        this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+          housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+        );
+    }
+}
